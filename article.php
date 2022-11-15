28cd0b9bcac8794ef24bcd00753eb1f583127cba
Azam Alamov <alamovazamjon@gmail.com>
2022-11-16 01:56:16 +0300

    Add files via upload

---
+++ article.php
@@ -0,0 +1,25 @@
+<?php include'header.php' ?>
+
+        <?php
+            $reqID = $_GET["id"];
+            $sql = "SELECT id, name, added_by, desc_short, desc_full, time_added FROM main WHERE id = $reqID";
+            $result = $conn->query($sql);
+            $row = $result->fetch_assoc()
+        ?>
+
+    <div class="uk-container uk-margin-top">
+
+        <h2 class="uk-margin-remove-bottom"><?php echo $row["name"] ?></h2>
+        <?php $tempDate = new DateTime($row["time_added"]); ?>
+        <p class="uk-margin-remove-top uk-margin-remove-bottom">by <?php echo $row["added_by"] ?>, <?php echo date_format($tempDate, "H:i, d.m.y") ?></p>
+
+        <p class="uk-margin-small-top"><i><?php echo $row["desc_short"] ?></i></p>
+
+        <p class="uk-text-emphasis"><?php echo $row["desc_full"] ?></p>
+
+    </div>
+
+
+    <?php include'footer.php' ?>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="uk-background-secondary uk-margin-medium-top">
+    <div class="uk-text-center uk-padding-small">
+        gthanksg, 2022
+    </div>
+</footer>
