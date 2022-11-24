e60e95b67bc810daecf079219718cf7b29fe4592
gthanksg <alamovazamjon@gmail.com>
2022-11-24 13:46:55 +0300

    refreshed search results page

--- results.php
+++ results.php
@@ -3,9 +3,13 @@
     $query = $_GET["query"];
 ?>
 
-<div class="uk-container sp-height">
+<div class="uk-height-large uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light" data-src="res/search.jpg" uk-img style="background-attachment: fixed">
+    <h1 class="text-shadow">Search results</h1>
+</div>
 
-    <h2 class="uk-margin-medium-top">Search results for query <i><?php echo $_GET["query"] ?></i></h2>
+<div class="uk-container uk-margin-top sp-height">
+
+    <div class="uk-grid-match uk-grid-divider" uk-grid>
 
     <?php $sql = "SELECT id, name, added_by, desc_short, desc_full, time_added FROM main WHERE name LIKE '%$query%' ORDER BY time_added desc";
     $result = $conn->query($sql);
@@ -24,23 +28,31 @@
         while($row = $result->fetch_assoc()) { ?>
 
     <?php $tempDate = new DateTime($row["time_added"]); ?>
-    <div class="uk-card uk-card-body uk-card-default uk-margin-bottom">
-        <article class="uk-article">
-            <h2 class="uk-article-title uk-margin-remove-bottom"><a class="uk-link-reset" href=""><?php echo $row["name"]?></a></h2>
-            <p class="uk-article-meta uk-margin-remove-top">by <i><a href="#"><?php echo $row["added_by"]?></a></i> on <?php echo date_format($tempDate, "H:i, d.m.y") ?></p>
-            <p class="uk-text-lead"><?php echo $row["desc_short"] ?></p>
-            <a class="uk-button uk-button-text" href="/article.php?id=<?php echo $row["id"] ?>">Read more</a>
-        </article>
-    </div>
+
+        <div class="uk-width-1-3">
+            <div class="uk-card uk-card-body uk-card-default uk-margin-bottom">
+                <h3 class="uk-margin-remove-bottom"><a class="uk-link-reset" href=""><?php echo $row["name"]?></a></h3>
+                <p class="uk-text-meta uk-margin-remove-top">by <i><a href="#"><?php echo $row["added_by"]?></a></i> on <?php echo date_format($tempDate, "H:i, d.m.y") ?></p>
+                <hr>
+                <p class="uk-text-meta"><?php echo $row["desc_short"] ?></p>
+                <a class="uk-button uk-button-text" href="/article.php?id=<?php echo $row["id"] ?>">Read more</a>
+            </div>
+        </div>
 
     <?php }
     } else { ?>
-        <div class="uk-position-absolute uk-position-center uk-width-1-1">
-            <div class="uk-tile uk-tile-secondary">
-                Nothing found.
+
+        <div class="uk-width-1-1">
+            <div class="uk-card uk-card-body uk-card-default uk-margin-bottom">
+                <h3 class="uk-margin-remove-bottom"><a class="uk-link-reset" href="">Nothing found.</a></h3>
+                <hr>
+                <p class="uk-text-meta">No results from your query "<?php echo $query?>"</p>
             </div>
         </div>
+
     <?php } ?>
+
+    </div>
 
 </div>
 
